a39258250abeb664fd77f3a1e0d8266e8af01a82
Asher <romanticasher@gmail.com>
2020-02-19 20:14:00 +0800

    添加防抖函数

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      scroll: null
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad
+    })
+
+    this.scroll.on('scroll', position => {
+      this.$emit('scroll', position)
+    })
+  },
+  methods: {
+    scrollTo (x, y, time = 300) {
+      this.scroll && this.scroll.scrollTo(x, y, time)
+    },
+    finishPullUp () {
+      this.scroll && this.scroll.finishPullUp()
+    },
+    refresh () {
+      this.scroll && this.scroll.refresh()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,138 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentScroll" :pull-up-load="true">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
+  </div>
+</template>
+
+<script>
+import HomeSwiper from "./childComps/HomeSwiper"
+import RecommendView from "./childComps/RecommendView"
+import FeatureView from "./childComps/FeatureView"
+
+import NavBar from "components/common/navbar/NavBar"
+import Scroll from "components/common/scroll/Scroll"
+import TabControl from "components/content/tabControl/TabControl"
+import GoodsList from "components/content/goods/GoodsList"
+import BackTop from "components/content/backTop/BackTop"
+
+import { getHomeMultidata, getHomeGoods } from "../../network/home"
+
+export default {
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      categories: ['pop', 'new', 'sell'],
+      currentCate: 0,
+      isShowBackTop: false
+    };
+  },
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    Scroll,
+    TabControl,
+    GoodsList,
+    BackTop
+  },
+  created() {
+    // 请求home头数据
+    this.getHomeMultidata();
+
+    // 请求商品数据
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+
+  },
+  mounted() {
+    // 监听item中图片加载完成
+    this.$bus.$on('itemImageLoad', () => {
+      this.$refs.scroll.refresh()
+    })
+  },
+  methods: {
+    // 事件监听相关方法
+    tabClick (index) {
+      console.log(this.categories[index])
+      
+      this.currentCate = index
+    },
+    backClick () {
+      this.$refs.scroll.scrollTo(0, 0, 500)
+    },
+    contentScroll (position) {
+      this.isShowBackTop = -position.y > 1000
+    },
+
+    // 网络请求相关方法
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page++
+      });
+    }
+  },
+  computed: {
+    showGoods () {
+      return this.goods[this.categories[this.currentCate]].list
+    }
+  }
+};
+</script>
+
+<style scoped>
+#home {
+  height: 100vh;
+  margin-top: 44px;
+}
+
+.home-nav {
+  width: 100%;
+  background-color: var(--color-tint);
+  color: #fff;
+
+  position: fixed;
+  left: 0;
+  top: 0;
+  left: 0;
+  z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  background-color: #fff;
+  z-index: 9;
+}
+
+.content {
+  height: calc(100% - 93px);
+  overflow: hidden;
+}
+</style>
